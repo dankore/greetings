f80298d1384a9e11d595d7b1521aa32dd556d57f
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2023-06-03 23:51:57 +0000

    return multiples map of name:message pair

--- hello/go.mod
+++ hello/go.mod
@@ -1,7 +1,7 @@
-module github.com/dankore/hello
+module hello.go
 
 go 1.20
 
-require github.com/dankore/handy-go-packages/greetings v0.0.0-20230603214255-ac9b5b3814d7
+require github.com/dankore/handy-go-packages/greetings v0.0.0-20230603230516-3242754c6356
 
 replace github.com/dankore/handy-go-packages/greetings => ../greetings
--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,19 @@ package main
 
 import "github.com/dankore/handy-go-packages/greetings"
 import "fmt"
+import "log"
 
 func main(){
-	message := greetings.Hello("Adamu")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	var names = []string {"Adamu", "Fatima", "Adam", "Kado", "Zahra"}
+
+	message, err:= greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(message)
 }
